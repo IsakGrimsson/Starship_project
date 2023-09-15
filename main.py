964756d44ed403e51594d7f1318890312935dd87
Tommy Ainsworth <tomainsworth99@gmail.com>
2023-09-15 10:23:51 +0100

    OOP set up for API connections and mongo connections, main.py changed for testing purposes

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 from API_pymongo import StarWarsAPI
 from API_pymongo import MongoDB
+import pymongo
+import requests
+
 
 sw = StarWarsAPI()
 mg = MongoDB()
-starships_data = sw.fetch_many_pages("starships", 4)
+starships_data = sw.fetch_all_pages("starships")
 
-mg.insert_data(starships_data, 'starwars', 'starships')
+new_starships_data = mg.replace_inner_url(starships_data, 'pilots')
+
+mg.insert_data(new_starships_data, 'starwars', 'starships')
+
 
 
 
