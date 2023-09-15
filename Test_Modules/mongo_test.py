3395744ad7437da8b097360c4c4af7d3dd56007a
Jack Cavanagh <jack.m.cavanagh@hotmail.co.uk>
2023-09-15 17:34:43 +0100

    added mongo test module and 3 unit tests

--- Test_Modules/mongo_test.py
+++ Test_Modules/mongo_test.py
@@ -1,11 +1,22 @@
 import unittest
-from API_pymongo import MongoDB
+from MongoDB_API import MongoDBAPI
 
 
 class TestAPIMethods(unittest.TestCase):
 
     def setUp(self) -> None:
-        self.MongoDB = MongoDB()
+        self.MongoDB = MongoDBAPI()
     def test_insert_data(self):
-        data_collection = self.MongoDB.insert_data([1, 2, 3, 4], "testdb", "test_collection")
-        self.assertIsNotNone(data_collection)
+        collection = self.MongoDB.create_collection("testdb", "test_collection")
+        self.assertIsNotNone(collection)
+
+    def test_insert_into_collection(self):
+        collection = self.MongoDB.create_collection("testdb", "test_collection")
+        self.MongoDB.insert_lst_into_db([{'1': 'one'}, {'2': 'two'}, {'3': 'three'}, {'4': 'four'}], collection)
+        actual = collection.count_documents({})
+        expected = 4
+        self.assertEqual(actual, expected)
+
+    def test_mongo_connection(self):
+        connection = self.MongoDB.connect_to_mongo('testdb')
+        self.assertIsNotNone(connection)
