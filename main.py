f8a127281ea0033b705ccc75bb05aa37978b0892
Taslima Hossain <htaslima@outlook.com>
2023-09-17 12:50:43 +0100

    Updated main.py and transformations.py with new functions

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from StarWars_API import StarWarsAPI
 from MongoDB_API import MongoDBAPI
-from transformations import transform_field_to_integer
+from transformations import transform_field_to_integer, clean_unknown, transform_field_to_float, clean_na, normalise_data
 
 sw = StarWarsAPI()
 mg = MongoDBAPI()
@@ -12,12 +12,32 @@ starships_data = sw.fetch_all_pages("starships")
 new_starships_data = sw.replace_url(starships_data, 'pilots', 'name')
 
 # Perform transformations
+clean_unknown(new_starships_data, 'consumables')
+clean_unknown(new_starships_data, 'hyperdrive_rating')
+clean_unknown(new_starships_data, 'cost_in_credits')
+clean_unknown(new_starships_data, 'cargo_capacity')
+clean_unknown(new_starships_data, 'MGLT')
+clean_unknown(new_starships_data, 'passengers')
+clean_unknown(new_starships_data, 'crew')
+clean_unknown(new_starships_data, 'max_atmosphering_speed')
+
+clean_na(new_starships_data, 'max_atmosphering_speed')
+clean_na(new_starships_data, 'passengers')
+
+normalise_data(new_starships_data, 'name')
+normalise_data(new_starships_data, 'model')
+normalise_data(new_starships_data, 'manufacturer')
+normalise_data(new_starships_data, 'starship_class')
+
+transform_field_to_float(new_starships_data, 'length')
+transform_field_to_float(new_starships_data, 'hyperdrive_rating')
+
 transform_field_to_integer(new_starships_data, 'crew')
 transform_field_to_integer(new_starships_data, 'cargo_capacity')
 transform_field_to_integer(new_starships_data, 'passengers')
 transform_field_to_integer(new_starships_data, 'cost_in_credits')
-transform_field_to_integer(new_starships_data, 'length')
 transform_field_to_integer(new_starships_data, 'max_atmosphering_speed')
+transform_field_to_integer(new_starships_data, 'MGLT')
 
 # Insert into database
 collection = mg.create_collection('starwars', 'starships')
--- transformations.py
+++ transformations.py
@@ -1,6 +1,5 @@
 from MongoDB_API import MongoDBAPI
 
-
 # Transforms numeric string fields to integers.
 def transform_field_to_integer(data_list, field_name):
     for item in data_list:
@@ -12,6 +11,49 @@ def transform_field_to_integer(data_list, field_name):
             except ValueError:
                 pass
 
+# Transforms numeric string fields to double.
+def transform_field_to_float(data_list, field_name):
+    for item in data_list:
+        if field_name in item:
+            original_value = item[field_name]
+            try:
+                new_value = float(str(original_value).replace(',', ''))
+                item[field_name] = new_value
+            except ValueError:
+                pass
+
+# Cleaning unknown value in empty string.
+def clean_unknown(data_list, field_name):
+    for item in data_list:
+        if field_name in item:
+            original_value = item[field_name]
+            try:
+                new_value = str(original_value).replace('unknown', '')
+                item[field_name] = new_value
+            except ValueError:
+                pass
+
+# Cleaning n/a value in empty string.
+def clean_na(data_list, field_name):
+    for item in data_list:
+        if field_name in item:
+            original_value = item[field_name]
+            try:
+                new_value = str(original_value).replace('n/a', '')
+                item[field_name] = new_value
+            except ValueError:
+                pass
+
+# Normalising the data by capitalising each first letter of a string.
+def normalise_data(data_list, field_name):
+    for item in data_list:
+        if field_name in item:
+            original_value = item[field_name]
+            try:
+                new_value = str(original_value).replace(',', '').title()
+                item[field_name] = new_value
+            except ValueError:
+                pass
 
 def perform_transformations():
     # Create a MongoDBAPI instance
@@ -30,8 +72,26 @@ def perform_transformations():
                            'passengers',
                            'cost_in_credits',
                            'length',
-                           'max_atmosphering_speed']
+                           'max_atmosphering_speed',
+                           'MGLT',
+                           'consumables',
+                           'hyperdrive_rating',
+                           'name',
+                           'model',
+                           'manufacturer',
+                           'starship_class']
 
+    for field in fields_to_transform:
+        clean_unknown(starships_collection, field)
+
+    for field in fields_to_transform:
+        clean_na(starships_collection, field)
+
+    for field in fields_to_transform:
+        transform_field_to_float(starships_collection, field)
+
+    for field in fields_to_transform:
+        normalise_data(starships_collection, field)
     # Transform specified fields to integers
     for field in fields_to_transform:
         transform_field_to_integer(starships_collection, field)
