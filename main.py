a970f21d1764dfa392d64ba4f3a3dcc41fcbc1ae
Taslima Hossain <htaslima@outlook.com>
2023-09-17 18:04:58 +0100

    Followed OOP structure for transformation.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from StarWars_API import StarWarsAPI
 from MongoDB_API import MongoDBAPI
-from transformations import transform_field_to_integer, clean_unknown, transform_field_to_float, clean_na, normalise_data
+from transformations import Transformations
 
 sw = StarWarsAPI()
 mg = MongoDBAPI()
+tr = Transformations()
 
 # Fetch the data
 starships_data = sw.fetch_all_pages("starships")
@@ -12,32 +13,32 @@ starships_data = sw.fetch_all_pages("starships")
 new_starships_data = sw.replace_url(starships_data, 'pilots', 'name')
 
 # Perform transformations
-clean_unknown(new_starships_data, 'consumables')
-clean_unknown(new_starships_data, 'hyperdrive_rating')
-clean_unknown(new_starships_data, 'cost_in_credits')
-clean_unknown(new_starships_data, 'cargo_capacity')
-clean_unknown(new_starships_data, 'MGLT')
-clean_unknown(new_starships_data, 'passengers')
-clean_unknown(new_starships_data, 'crew')
-clean_unknown(new_starships_data, 'max_atmosphering_speed')
+tr.clean_unknown(new_starships_data, 'consumables')
+tr.clean_unknown(new_starships_data, 'hyperdrive_rating')
+tr.clean_unknown(new_starships_data, 'cost_in_credits')
+tr.clean_unknown(new_starships_data, 'cargo_capacity')
+tr.clean_unknown(new_starships_data, 'MGLT')
+tr.clean_unknown(new_starships_data, 'passengers')
+tr.clean_unknown(new_starships_data, 'crew')
+tr.clean_unknown(new_starships_data, 'max_atmosphering_speed')
 
-clean_na(new_starships_data, 'max_atmosphering_speed')
-clean_na(new_starships_data, 'passengers')
+tr.clean_na(new_starships_data, 'max_atmosphering_speed')
+tr.clean_na(new_starships_data, 'passengers')
 
-normalise_data(new_starships_data, 'name')
-normalise_data(new_starships_data, 'model')
-normalise_data(new_starships_data, 'manufacturer')
-normalise_data(new_starships_data, 'starship_class')
+tr.normalise_data(new_starships_data, 'name')
+tr.normalise_data(new_starships_data, 'model')
+tr.normalise_data(new_starships_data, 'manufacturer')
+tr.normalise_data(new_starships_data, 'starship_class')
 
-transform_field_to_float(new_starships_data, 'length')
-transform_field_to_float(new_starships_data, 'hyperdrive_rating')
+tr.transform_field_to_float(new_starships_data, 'length')
+tr.transform_field_to_float(new_starships_data, 'hyperdrive_rating')
 
-transform_field_to_integer(new_starships_data, 'crew')
-transform_field_to_integer(new_starships_data, 'cargo_capacity')
-transform_field_to_integer(new_starships_data, 'passengers')
-transform_field_to_integer(new_starships_data, 'cost_in_credits')
-transform_field_to_integer(new_starships_data, 'max_atmosphering_speed')
-transform_field_to_integer(new_starships_data, 'MGLT')
+tr.transform_field_to_integer(new_starships_data, 'crew')
+tr.transform_field_to_integer(new_starships_data, 'cargo_capacity')
+tr.transform_field_to_integer(new_starships_data, 'passengers')
+tr.transform_field_to_integer(new_starships_data, 'cost_in_credits')
+tr.transform_field_to_integer(new_starships_data, 'max_atmosphering_speed')
+tr.transform_field_to_integer(new_starships_data, 'MGLT')
 
 # Insert into database
 collection = mg.create_collection('starwars', 'starships')
--- transformations.py
+++ transformations.py
@@ -1,102 +1,103 @@
 from MongoDB_API import MongoDBAPI
 
+class Transformations(MongoDBAPI):
 # Transforms numeric string fields to integers.
-def transform_field_to_integer(data_list, field_name):
-    for item in data_list:
-        if field_name in item:
-            original_value = item[field_name]
-            try:
-                new_value = int(str(original_value).replace(',', ''))
-                item[field_name] = new_value
-            except ValueError:
-                pass
+    def transform_field_to_integer(self, data_list, field_name):
+        for item in data_list:
+            if field_name in item:
+                original_value = item[field_name]
+                try:
+                    new_value = int(str(original_value).replace(',', ''))
+                    item[field_name] = new_value
+                except ValueError:
+                    pass
 
-# Transforms numeric string fields to double.
-def transform_field_to_float(data_list, field_name):
-    for item in data_list:
-        if field_name in item:
-            original_value = item[field_name]
-            try:
-                new_value = float(str(original_value).replace(',', ''))
-                item[field_name] = new_value
-            except ValueError:
-                pass
+    # Transforms numeric string fields to double.
+    def transform_field_to_float(self, data_list, field_name):
+        for item in data_list:
+            if field_name in item:
+                original_value = item[field_name]
+                try:
+                    new_value = float(str(original_value).replace(',', ''))
+                    item[field_name] = new_value
+                except ValueError:
+                    pass
 
-# Cleaning unknown value in empty string.
-def clean_unknown(data_list, field_name):
-    for item in data_list:
-        if field_name in item:
-            original_value = item[field_name]
-            try:
-                new_value = str(original_value).replace('unknown', '')
-                item[field_name] = new_value
-            except ValueError:
-                pass
+    # Cleaning unknown value in empty string.
+    def clean_unknown(self, data_list, field_name):
+        for item in data_list:
+            if field_name in item:
+                original_value = item[field_name]
+                try:
+                    new_value = str(original_value).replace('unknown', '')
+                    item[field_name] = new_value
+                except ValueError:
+                    pass
 
-# Cleaning n/a value in empty string.
-def clean_na(data_list, field_name):
-    for item in data_list:
-        if field_name in item:
-            original_value = item[field_name]
-            try:
-                new_value = str(original_value).replace('n/a', '')
-                item[field_name] = new_value
-            except ValueError:
-                pass
+    # Cleaning n/a value in empty string.
+    def clean_na(self, data_list, field_name):
+        for item in data_list:
+            if field_name in item:
+                original_value = item[field_name]
+                try:
+                    new_value = str(original_value).replace('n/a', '')
+                    item[field_name] = new_value
+                except ValueError:
+                    pass
 
-# Normalising the data by capitalising each first letter of a string.
-def normalise_data(data_list, field_name):
-    for item in data_list:
-        if field_name in item:
-            original_value = item[field_name]
-            try:
-                new_value = str(original_value).replace(',', '').title()
-                item[field_name] = new_value
-            except ValueError:
-                pass
+    # Normalising the data by capitalising each first letter of a string.
+    def normalise_data(self, data_list, field_name):
+        for item in data_list:
+            if field_name in item:
+                original_value = item[field_name]
+                try:
+                    new_value = str(original_value).replace(',', '').title()
+                    item[field_name] = new_value
+                except ValueError:
+                    pass
 
-def perform_transformations():
-    # Create a MongoDBAPI instance
-    mongodb_api = MongoDBAPI()
+    def perform_transformations(self):
+        # Create a MongoDBAPI instance
+        mongodb_api = MongoDBAPI()
 
-    # Specify database and collection names
-    database_name = 'starwars'
-    collection_name = 'starships'
+        # Specify database and collection names
+        database_name = 'starwars'
+        collection_name = 'starships'
 
-    # Connect to "starships" collection
-    starships_collection = mongodb_api.connect_to_mongo(database_name)[collection_name]
+        # Connect to "starships" collection
+        starships_collection = mongodb_api.connect_to_mongo(database_name)[collection_name]
 
-    # List fields for transformation
-    fields_to_transform = ['crew',
-                           'cargo_capacity',
-                           'passengers',
-                           'cost_in_credits',
-                           'length',
-                           'max_atmosphering_speed',
-                           'MGLT',
-                           'consumables',
-                           'hyperdrive_rating',
-                           'name',
-                           'model',
-                           'manufacturer',
-                           'starship_class']
+        # List fields for transformation
+        fields_to_transform = ['crew',
+                               'cargo_capacity',
+                               'passengers',
+                               'cost_in_credits',
+                               'length',
+                               'max_atmosphering_speed',
+                               'MGLT',
+                               'consumables',
+                               'hyperdrive_rating',
+                               'name',
+                               'model',
+                               'manufacturer',
+                               'starship_class']
 
-    for field in fields_to_transform:
-        clean_unknown(starships_collection, field)
+        for field in fields_to_transform:
+            self.clean_unknown(starships_collection, field)
 
-    for field in fields_to_transform:
-        clean_na(starships_collection, field)
+        for field in fields_to_transform:
+            self.clean_na(starships_collection, field)
 
-    for field in fields_to_transform:
-        transform_field_to_float(starships_collection, field)
+        for field in fields_to_transform:
+            self.transform_field_to_float(starships_collection, field)
 
-    for field in fields_to_transform:
-        normalise_data(starships_collection, field)
-    # Transform specified fields to integers
-    for field in fields_to_transform:
-        transform_field_to_integer(starships_collection, field)
+        for field in fields_to_transform:
+            self.normalise_data(starships_collection, field)
+        # Transform specified fields to integers
+        for field in fields_to_transform:
+            self.transform_field_to_integer(starships_collection, field)
 
 
-if __name__ == "__main__":
-    # Perform transformations
-    perform_transformations()
+    if __name__ == "__main__":
+    #Perform transformations
+        perform_transformations()
